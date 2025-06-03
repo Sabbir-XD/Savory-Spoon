cf1c7b8434ab0f8380fa76fd73c919c5f1524609
Sabbir-96 <sabbirislam9404@gmail.com>
2025-06-03 23:27:49 +0600

    full project setup complete

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import AllFoods from "../pages/AllFoods";
+import Gallery from "../pages/Gallery";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/AllFoods",
+        element: <AllFoods />,
+      },
+      {
+        path: "/Gallery",
+        element: <Gallery />,
       },
     ],
   },
